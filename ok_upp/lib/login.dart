11b0f69767eaa1da35a9ecc4a2f78dc8ccf775a7
Choy050823 <choymh23@gmail.com>
2022-03-08 08:44:38 +0800

    Google Sign In (No Error)

--- ok_upp/lib/login.dart
+++ ok_upp/lib/login.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({ Key? key }) : super(key: key);
@@ -11,8 +11,17 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Center(
+      child: ElevatedButton.icon(
+        style: ElevatedButton.styleFrom(
+          primary: Colors.white,
+          onPrimary: Colors.black,
+          minimumSize: const Size(double.infinity, 50),
+        ),
+        icon: const FaIcon(FontAwesomeIcons.google, color: Colors.red),
+        label: const Text("Sign Up with Google"),
+        onPressed: () {},
+      ),
     );
   }
 }
